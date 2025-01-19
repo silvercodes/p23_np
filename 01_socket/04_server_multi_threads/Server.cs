50005f8e0ec2edf624d3a13c5145732e9d4b2d4c
Vladimir Serebrianskiy <silver.itstep@gmail.com>
2025-01-19 15:29:51 +0300

    add: server with Tasks

--- 01_socket/04_server_multi_threads/Server.cs
+++ 01_socket/04_server_multi_threads/Server.cs
@@ -1,16 +1,103 @@
-﻿namespace _04_server_multi_threads;
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
-internal class Server
+namespace _04_server_multi_threads;
+
+internal class Server: IAsyncDisposable
 {
+    private int backlog;
     public string Ip { get; }
     public int Port { get; }
-    public Server(string ip, int port)
+    public Socket ServerSocket { get; private set; }
+    public Server(string ip, int port, int backlog = 10)
     {
         Ip = ip;
         Port = port;
+
+        ServerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        IPEndPoint ep = new IPEndPoint(IPAddress.Parse(Ip), Port);
+        ServerSocket.Bind(ep);
     }
+    
     public async Task StartAsync()
     {
+        try
+        {
+            ServerSocket.Listen(backlog);
+            await Console.Out.WriteLineAsync($"Server started at {Ip}:{Port}");
 
+            await HandleAsync();
+        }
+        catch (Exception ex)
+        {
+            await Console.Out.WriteLineAsync($"ERROR: {ex.Message}");
+        }
+    }
+    public async Task HandleAsync()
+    {
+        while(true)
+        {
+            Socket remoteSocket = await ServerSocket.AcceptAsync();
+
+            if (remoteSocket.RemoteEndPoint is IPEndPoint remoteEP)
+                await Console.Out.WriteLineAsync($"Connection opened for: {remoteEP.Address}:{remoteEP.Port}");
+
+            _ = Task.Run(() => HandleConnection(remoteSocket));
+        }
+    }
+
+
+    private void HandleConnection(Socket remoteSocket)
+    {
+        try
+        {
+            while (true)
+            {
+                string message = ReadMessage(remoteSocket);
+
+                Console.WriteLine($"{DateTime.Now.ToShortTimeString()} -> {message}");
+
+                Thread.Sleep(1000);
+
+                string response = $"OK ({message})";
+                remoteSocket.Send(Encoding.UTF8.GetBytes(response));
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Exception in ConnectionHandler: {ex.Message}");
+        }
+        finally
+        {
+            remoteSocket.Shutdown(SocketShutdown.Both);
+            remoteSocket.Close();
+
+            Console.WriteLine("Connection closed...");
+        }
+    }
+    string ReadMessage(Socket remoteSocket)
+    {
+        byte[] buffer = new byte[1024];
+        int byteCount = 0;
+        string message = string.Empty;
+
+        do
+        {
+            byteCount = remoteSocket.Receive(buffer);       // BLOCKING
+            message += Encoding.UTF8.GetString(buffer, 0, byteCount);
+
+        } while (remoteSocket.Available > 0);
+
+        return message;
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        await Task.Run(() =>
+        {
+            if (ServerSocket != null)
+                ServerSocket.Dispose();
+        });
     }
 }
---
+++ 01_socket/05_client_with_cycle/Program.cs
@@ -0,0 +1,53 @@
+﻿
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+const string serverIp = "127.0.0.1";
+const int serverPort = 8080;
+
+using Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse(serverIp), serverPort);
+
+try
+{
+    socket.Connect(serverEndPoint);                 // BLOCKING
+
+    while (true)
+    {
+        Console.Write("> ");
+        string? message = Console.ReadLine();
+
+        socket.Send(Encoding.UTF8.GetBytes(message));
+
+        string response = ReadMessage(socket);
+        Console.WriteLine($"Response: {response}");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+finally
+{
+    socket.Shutdown(SocketShutdown.Both);
+    socket.Close();
+}
+
+string ReadMessage(Socket remoteSocket)
+{
+    byte[] buffer = new byte[1024];
+    int byteCount = 0;
+    string message = string.Empty;
+
+    do
+    {
+        byteCount = remoteSocket.Receive(buffer);       // BLOCKING
+        message += Encoding.UTF8.GetString(buffer, 0, byteCount);
+
+    } while (remoteSocket.Available > 0);
+
+    return message;
+}
+
